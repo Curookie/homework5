46981cf44dfaccc030a623f7b8ba37939bc7e560
Curookie <curookie@naver.com>
2016-10-14 03:36:07 +0900

    하나의 java 파일에 클래스 파일을 모으고 SumOfNumbers 클래스 작성

--- homework5/src/homework5/ExamForWhile.java
+++ homework5/src/homework5/ExamForWhile.java
@@ -1,5 +1,7 @@
 package homework5;
 
+import java.util.Scanner;
+
 public class ExamForWhile {
 
 	public static void main(String[] args) {
@@ -9,4 +11,34 @@ public class ExamForWhile {
 		
 	}
 
+}
+
+class SumOfNumbers {
+	void sum() {
+		Scanner s = new Scanner(System.in);
+		System.out.print("마지막 수를 입력(Q:종료) >> ");
+		int num = s.nextInt();
+		int sum = 0;
+		
+		for(int i=1; i<=num; i++) {
+			sum += i;
+		}
+		System.out.println("총 합은 "+sum+"입니다.");
+	}
+}
+
+class MaxAndMin {
+	
+}
+
+class SumAndAvg {
+	
+}
+
+class GuGuDan {
+	
+}
+
+class GuGuDan2 {
+	
 }
